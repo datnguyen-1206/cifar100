6c7d55b2ff315b41dd54708fbe611d630bc164fe
datnguyen-1206 <sliderboo2005@gmail.com>
2024-05-15 19:47:43 +0700

    change dataloader

--- data_loader/get_cifar100.py
+++ data_loader/get_cifar100.py
@@ -8,22 +8,22 @@ def get_cifar100(data_augmentation='basic', batch_size=128, num_workers=4):
     # Data Augmentation
     if data_augmentation == 'basic':
         transform_train = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(227),
+            transforms.Resize(64),
+            transforms.CenterCrop(32),
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
         ])
         transform_test = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(227),
+            transforms.Resize(64),
+            transforms.CenterCrop(32),
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
         ])
 
     elif data_augmentation == 'sota': 
         transform_train = transforms.Compose([
-            transforms.Resize(256),
-            transforms.RandomCrop(227),
+            transforms.Resize(64),
+            transforms.RandomCrop(32),
             transforms.RandomHorizontalFlip(), 
             transforms.RandomVerticalFlip(),    
             transforms.RandomRotation(degrees=30), 
@@ -32,16 +32,16 @@ def get_cifar100(data_augmentation='basic', batch_size=128, num_workers=4):
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), 
         ])
         transform_test = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(227),
+            transforms.Resize(64),
+            transforms.CenterCrop(32),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), 
         ])
 
     elif data_augmentation == 'advanced':
         transform_train = transforms.Compose([
-            transforms.Resize(256),
-            transforms.RandomCrop(227),
+            transforms.Resize(64),
+            transforms.RandomCrop(32),
             transforms.RandomHorizontalFlip(),
             transforms.RandomVerticalFlip(),    
             transforms.RandomRotation(degrees=30),  
@@ -52,8 +52,8 @@ def get_cifar100(data_augmentation='basic', batch_size=128, num_workers=4):
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), 
         ])
         transform_test = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(227),
+            transforms.Resize(64),
+            transforms.CenterCrop(32),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), 
         ])
